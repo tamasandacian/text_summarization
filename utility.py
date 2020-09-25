d21ce4cf51b9f4ba7f29b4892ab7564aa9650bd3
Dacian Tamasan <daciantamasan@MacBook-Pro.local>
2020-09-25 17:17:13 +0200

    added utility methods

---
+++ utility.py
@@ -0,0 +1,50 @@
+import re
+
+class Utility(object):
+
+    @staticmethod
+    def clean_text(text):
+        """ Clean textual content """
+        text = Utility.lowercase(text)
+        text = Utility.remove_newlines(text)
+        text = Utility.remove_tabs(text)
+        text = Utility.replace_special_chars(text)
+        text = Utility.remove_multiple_whitespaces(text)
+        text = Utility.remove_trailing_whitespaces(text)
+        return text
+    
+    @staticmethod
+    def lowercase(text):
+        """ Lowercase text """
+        return text.lower()
+
+    @staticmethod
+    def remove_newlines(text):
+        """ Remove newlines characters from text """
+        return text.replace("\n", "")
+    
+    @staticmethod
+    def remove_tabs(text):
+        """ Remove tab characters from text """
+        return text.replace("\t", "")
+    
+    @staticmethod
+    def replace_special_chars(text):
+        """ Remove special characters from text """
+        return re.sub(r'\W+', ' ', text)
+    
+    @staticmethod
+    def remove_multiple_whitespaces(text):
+        """ Remove whitespaces from text """
+        return re.sub(r'\s+', ' ', text, flags=re.I)
+
+    @staticmethod
+    def remove_trailing_whitespaces(text):
+        """ Remove whitespaces from beggining and ending text """
+        return text.strip()
+   
+    @staticmethod
+    def get_doc_length(text):
+        """ Determine number of words in a document."""
+        doc_length = len(re.findall(r'\w+', text))
+        return doc_length
